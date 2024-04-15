83907b681823d74971ed3eb95f4eac9e8556e6d4
davidxv15 <davidxvaz@gmail.com>
2024-04-15 13:12:25 -0700

    h1 edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Calendar from "./components/Calendar/Calendar";
 function App() {
   return (
     <div className="App">
-      <h1>Nutrition <span className="cursive">Calendar1.0!</span></h1>
+      <h1>Nutrition <span className="cursive">Calendar1.0</span></h1>
       <Calendar />
     </div>
   );
